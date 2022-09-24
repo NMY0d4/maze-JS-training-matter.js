3938906251243b8023df8d370a617447ca0cf0b2
Grégory Marini <greg.marini@hotmail.fr>
2022-09-24 14:48:38 +0200

    crétion des tables pour la crétion du labyrinthe

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 const borderSize = 40;
@@ -48,3 +49,19 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// Maze generation
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
