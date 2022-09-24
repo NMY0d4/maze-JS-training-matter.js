d3cbc501e8eb40e6465f2078c60124b3473faadd
Grégory Marini <greg.marini@hotmail.fr>
2022-09-24 12:10:55 +0200

    création d'une petite démo pour comprendre les bases de la bibliothèque

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
+    Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,16 +10,72 @@ const render = Render.create({
     element: document.body,
     engine,
     options: {
-        width: 800,
-        height: 600,
+        wireframes: false,
+        width,
+        height,
     },
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true,
-});
+World.add(
+    world,
+    MouseConstraint.create(engine, {
+        mouse: Mouse.create(render.canvas),
+    })
+);
 
-World.add(world, shape);
+// Walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, {
+        isStatic: true,
+        render: {
+            fillStyle: "red",
+        },
+    }),
+    Bodies.rectangle(800, 300, 40, 600, {
+        isStatic: true,
+        render: {
+            fillStyle: "red",
+        },
+    }),
+    Bodies.rectangle(400, 600, 800, 40, {
+        isStatic: true,
+        render: {
+            fillStyle: "red",
+        },
+    }),
+    Bodies.rectangle(0, 300, 40, 600, {
+        isStatic: true,
+        render: {
+            fillStyle: "red",
+        },
+    }),
+];
+
+World.add(world, walls);
+
+// Random Shapes
+for (let i = 0; i < 50; i++) {
+    if (Math.random() > 0.5) {
+        World.add(
+            world,
+            Bodies.rectangle(
+                Math.random() * width,
+                Math.random() * height,
+                50,
+                50
+            )
+        );
+    } else {
+        World.add(
+            world,
+            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+                render: {
+                    fillStyle: "green",
+                },
+            })
+        );
+    }
+}
